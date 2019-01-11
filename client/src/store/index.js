f56f4c970c47535bfcfcf882a1e295a612b72e34
Batuhan Tasdoven <btasdoven@gmail.com>
2019-01-11 09:10:20 +0000

    fix for env vars - 2

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import thunk from 'redux-thunk';
 
-import { envService } from '../services/env.service'
+import envService from '../services/env.service'
 import rootReducer from "./reducers";
 
 export default envService.isProduction 
