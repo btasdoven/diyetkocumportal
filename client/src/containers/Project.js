98b8922e94e5255c749afe3f5a365e4b5891717c
Batuhan Tasdoven <batasdov@microsoft.com>
2019-07-26 02:09:50 -0700

    preview taken picture in dialog

--- client/src/containers/Project.js
+++ client/src/containers/Project.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Camera from 'react-html5-camera-photo';
 import 'react-html5-camera-photo/build/css/index.css';
 import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
 const styles = theme => ({
   card: {
@@ -10,14 +11,41 @@ const styles = theme => ({
 });
 
 class CameraWrapper extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {}
+
+    this.onTakePhoto = this.onTakePhoto.bind(this);
+  }
+
+  onTakePhoto() {
+    this.props.onTakePhoto(this.state.url)
+  }
+
   render () {
     return (
-      <Camera
-        className={this.props.classes.card}
-        component={<span />}
-        isFullscreen={true}
-        onTakePhoto = { (dataUri) => { this.props.onTakePhoto(dataUri); } }
-      />
+      <span>
+        { this.state.url == undefined && 
+          <Camera
+            className={this.props.classes.card}
+            idealFacingMode="environment"
+            isFullscreen={false}
+            onTakePhoto = { (dataUri) => { this.setState({url: dataUri}) } }
+          />
+        }
+
+        { this.state.url && 
+          (
+            <div>
+              <img src={this.state.url} /> 
+              <Button onClick={this.onTakePhoto.bind(this)}>OK</Button>
+              <Button onClick={() => this.setState({url: undefined})}>Retry</Button>
+            </div>
+          )
+        }
+      </span>
     );
   }
 }
